7a9181fdabe09615909396239b6df7f0ef790959
Venura Athukorala <venura9@users.noreply.github.com>
2024-11-17 14:17:40 +1100

    removed the variables to be updated to variables.tf

--- aci/main.tf
+++ aci/main.tf
@@ -1,19 +1,18 @@
 terraform {
-  required_providers {
-     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>3.0.0"
-    } 
-  }
   required_version = "~> 1.9.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
 }
-
 provider "azurerm" {
   features {}
-}
-
-variable "prefix" {
-  default = "terraform"
 }
 
 resource "azurerm_resource_group" "rg" {
