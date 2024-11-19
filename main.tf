4e87039620dc7508a50d85c66e8e4c381ca5b6f5
Venura Athukorala <venura9@users.noreply.github.com>
2024-11-19 22:19:54 +1100

    added the storage accounts in preperation for caddy https

--- main.tf
+++ main.tf
@@ -49,3 +49,17 @@ resource "azurerm_container_group" "container" {
     }
   }
 }
+
+resource "azurerm_storage_account" "caddy_storage" {
+  name                      = "${var.container_group_name_prefix}-${random_string.container_name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+}
+
+resource "azurerm_storage_share" "caddy_share_data" {
+  name                 = "caddy_share_data"
+  storage_account_name = azurerm_storage_account.caddy_storage.name
+}
