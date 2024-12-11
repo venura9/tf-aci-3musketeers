1c96600725f1120420c29b3a7794402061973eca
Venura Athukorala <venura9@users.noreply.github.com>
2024-12-12 08:35:18 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "~>3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>4.0"
+      version = "~>3.0"
     }
   }
   cloud{}
